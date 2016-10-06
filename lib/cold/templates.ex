e641c1beb545254b292ed4795144ed863969fb35
Shane Kilkelly <shane@kilkelly.me>
2016-10-06 18:47:40 +0100

    Cruft

--- lib/cold/templates.ex
+++ lib/cold/templates.ex
@@ -1,27 +1,26 @@
 defmodule Cold.Templates do
 
   def homepage(%{timestamp: timestamp}) do
-    layout(
-      [
-        [:h1, "Hello, I have a cold"],
-        [:p,
-         "This is a demo of ",
-         [:a, %{href: "https://github.com/ShaneKilkelly/sneeze", target: "_blank"}, "sneeze"],
-          " the template data->html renderer."],
-        [:p,
-         "The current time is #{timestamp}"]
-      ])
+    layout [
+      [:h1, "Hello, I have a cold"],
+      [:p,
+        "This is a demo of ",
+        [:a, %{href: "https://github.com/ShaneKilkelly/sneeze", target: "_blank"}, "sneeze"],
+        " the template data->html renderer."],
+      [:p,
+        "The current time is #{timestamp}"]
+    ]
   end
 
   def layout(content_block) do
-    Sneeze.render([
+    Sneeze.render [
       [:__@raw_html, "<!DOCTYPE html>"],
       [:head,
        [:title, "Cold, Sneeze!"]],
       [:body,
        [:div, %{id: "main-content"},
         content_block]]
-    ])
+    ]
   end
 
 end
